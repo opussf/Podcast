bba91ad77a1a129c89e318a66e6d14c4f32431b7
opussf <github@zz9-za.com>
2020-06-09 16:05:01 -0700

    Initial commit
    Happiness depends upon ourselves.

---
+++ Podcast/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Podcast
+//
+//  Created by opussf on 6/9/2020.
+//  Copyright © 2020 opussf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
